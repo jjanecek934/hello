9f5947944e090d8a6c30b6cf13105528fadef4af
Jonny Janecek <jjanecek2@huskers.unl.edu>
2022-08-25 17:25:46 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/* ""
+Name: Jonny Janecek
+NUID: 67951692
+Date:
+Description:
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (void) {
+
+    printf("Jonny Janecek\n");
+
+    return 0;
+}
